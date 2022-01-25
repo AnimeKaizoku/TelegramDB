7c7c5b191b52e382ef03be01b4b0bc5f811389ba
Stark Programmer <88478059+StarkBotsIndustries@users.noreply.github.com>
2022-01-25 22:21:04 +0530

    Fix typo in deprecation warning (#7)

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -1,4 +1,4 @@
 import warnings
-warnings.warn("Use of 'telegram' for importing is depricated! Please use 'telegramdb' instead.", DeprecationWarning,
+warnings.warn("Use of 'telegram' for importing is deprecated! Please use 'telegramdb' instead.", DeprecationWarning,
               stacklevel=2)
 from telegramdb import *
